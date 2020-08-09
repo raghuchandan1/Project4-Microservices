d3a51432f30085ad784c01675899830ff188bc46
Raghu Chandan <raghuchandan1@gmail.com>
2020-08-09 22:20:04 +0530

    Finished kubernetes local deployment

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,28 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=raghuchandan1/devops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deployment devops --image=raghuchandan1/devops
 
+# Verification
+kubectl get deployments
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl expose deployment devops --type=LoadBalancer --port=80
 
+# Verification
+kubectl get services
+
+# Start service
+minikube service devops
+
+#Forward from port 80 to 8000
+kubectl port-forward deployment/devops 8000:80
